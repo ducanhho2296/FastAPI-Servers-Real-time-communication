def127a7ee92813fac73c80c5cc2ddd4aa17f7b0
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-03-05 10:29:27 +0100

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -1,108 +1,70 @@
-from fastapi import FastAPI
-import uvicorn
 import cv2
+import uvicorn
+import numpy as np
+from fastapi import FastAPI, Response, Request
+from fastapi.responses import StreamingResponse
+import sys
 
 app = FastAPI()
-cap = cv2.VideoCapture(0)
-
-@app.post("/start")
-async def start_inference():
-    print('Start model inference')
- 
-    pass
+camera = cv2.VideoCapture(0)
+streaming = False
 
 
-@app.post("/stop")
-async def stop_inference():
-    print('Stop model inference')
+def generate_frames():
+    while streaming:
+        success, frame = camera.read()
+        global img
+        img = frame.copy()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            # yield img
+
+@app.get('/')
+async def index():
+    return {"message": "Welcome to the webcam streaming application!"}
 
 
+@app.get('/video_feed')
+async def video_feed():
+    global streaming
+    streaming = True
+    return StreamingResponse(generate_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
 
-def generate():
-    while True: # TODO: add logic to stop camera here
-        ret, image = cap.read()
-        if image is not None:
-            retval, image_encoded = cv2.imencode(".jpg", image)
+@app.get('/stop_stream')
+async def stop_stream():
+    global streaming, camera
+    streaming = False
+    if camera:
+        camera.release()
+        camera = None
+    return {"message": "Streaming stopped successfully."}
 
-            if image_encoded is not None:
-                frame = image_encoded.tobytes()
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+@app.get('/restart_stream')
+async def restart_stream():
+    global streaming
+    streaming = True
+    return {"message": "Streaming restarted successfully."}
 
-
-@app.get("/preview")
-async def get_images():
-    return StreamingResponse(generate(), media_type="multipart/x-mixed-replace;boundary=frame")
-
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-from fastapi.responses import HTMLResponse
-
-app.mount("/", StaticFiles(directory="static"), name="static")
-
-@app.get("/stop-streaming.js")
-async def read_item():
-    return FileResponse("static/stop-streaming.js")
-
-app.mount("/", StaticFiles(directory="static"), name="static")
-
-@app.get("/", response_class=HTMLResponse)
-async def read_item():
-    return """
-    <html>
-        <body>
-            <video autoplay></video>
-            <button id="stopButton">Stop Streaming</button>
-            <script src="/docs/swagger-ui/stop-streaming.js"></script>
-            <script>
-                const stopButton = document.getElementById('stopButton');
-                
-                stopButton.addEventListener('click', () => {
-                  stopStreaming();
-                });
-                
-                const constraints = {
-                    audio: false,
-                    video: true
-                };
-                
-                navigator.mediaDevices.getUserMedia(constraints)
-                    .then((stream) => {
-                        const video = document.querySelector('video');
-                        video.srcObject = stream;
-                        video.onloadedmetadata = (e) => {
-                            video.play();
-                        };
-                    })
-                    .catch((err) => {
-                        console.log(err.name + ": " + err.message);
-                    });
-            </script>
-        </body>
-    </html>
-    """
+@app.get('/save_frame')
+async def save_frame(filename:str):
+    cv2.imwrite(filename, img)
 
 
+####demo using @app.post to receive request from client(web browser)
+@app.post('/demo')
+async def demo_getvalue(value:str):
+    global demo_value 
+    demo_value = value
+    return demo_value
 
+@app.get('/rundemo')
+async def rundemo():
+    return {"message": "the value was received as value = {}".format(demo_value)}
 
-
-from fastapi.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
-if __name__ == "__main__":
-    text = "OpenAPI Swagger link"
-    target = "http://127.0.0.1:8000/preview"  ## CLICK HERE
-    print(f"\u001b{text}\u001b\\{target}\u001b\u001b")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-    origins = ["*",
-                "http://localhost",
-                "http://localhost:8000",
-                ]
-
-    app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    )
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
