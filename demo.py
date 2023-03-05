01f41e8a01cfed04a243510dfc47d1f5a444048e
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-03-05 10:12:24 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,3 +3,23 @@ import numpy as np
 from fastapi import FastAPI, Response, Request
 from fastapi.responses import StreamingResponse
 import sys
+
+app = FastAPI()
+camera = cv2.VideoCapture(0)
+streaming = False
+
+
+def generate_frames():
+    while streaming:
+        success, frame = camera.read()
+        global img
+        img = frame.copy()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            # yield img
+
