840c44dcf5c915796ae8b2a719a9e09889048927
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-03-05 11:26:00 +0100

    update

--- demo_websocket.py
+++ demo_websocket.py
@@ -1,41 +1,63 @@
 import cv2
-import uvicorn
+import asyncio
 import numpy as np
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from starlette.websockets import WebSocketDisconnect
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
-# Mount the "static" directory as a static file directory
-app.mount("/static", StaticFiles(directory="static"), name="static")
+async def video_feed(websocket: WebSocket):
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise RuntimeError("Could not start camera.")
 
-# Define the HTML page served by the root URL
-@app.get("/")
-async def read_root():
-    with open("index.html") as f:
-        html = f.read()
-    return HTMLResponse(content=html, status_code=200)
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-# Define a WebSocket endpoint to stream video to the web browser
+            # Resize frame to reduce network bandwidth usage
+            frame = cv2.resize(frame, (640, 480))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            _, jpeg = cv2.imencode('.jpg', frame)
+
+            # Send frame over WebSocket
+            await websocket.send_bytes(jpeg.tobytes())
+    finally:
+        cap.release()
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    capture = cv2.VideoCapture(0)
-    while True:
-        ret, frame = capture.read()
-        if not ret:
-            break
-        try:
-            # Convert the frame to JPEG format
-            _, jpeg = cv2.imencode(".jpg", frame)
-            jpeg_bytes = jpeg.tobytes()
-            # Send the frame to the web browser
-            await websocket.send_bytes(jpeg_bytes)
-        except WebSocketDisconnect:
-            break
-    capture.release()
+    await video_feed(websocket)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/")
+async def read_root():
+    return StreamingResponse(video_feed_generator(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+def video_feed_generator():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise RuntimeError("Could not start camera.")
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Resize frame to reduce network bandwidth usage
+            frame = cv2.resize(frame, (640, 480))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            _, jpeg = cv2.imencode('.jpg', frame)
+
+            # Yield frame to browser
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+    finally:
+        cap.release()
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000)
