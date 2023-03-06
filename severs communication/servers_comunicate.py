94afee82964a251d88d89917f8bff7d167de3b1d
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2023-03-06 11:30:45 +0100

    update

--- severs communication/servers_comunicate.py
+++ severs communication/servers_comunicate.py
@@ -8,10 +8,10 @@ async def root():
     return {"message": "This is server 1"}
 
 app2 = FastAPI()
-@app.get('/')
+@app2.get('/')
 async def root():
     return {"message": "This is server 2"}
 
-if '__name__' == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    uvicorn.run(app2, host="0.0.0.1", port=8000)
+if __name__ == '__main__':
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app2, host="0.0.0.0", port=8000)
