30a8fe4fedbe62d96af9e205b7caf8f4f5695a84
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2023-03-07 12:11:16 +0100

    Update server1.py

--- servers communication/server1.py
+++ servers communication/server1.py
@@ -7,6 +7,7 @@ app = FastAPI()
 # Define the database connection settings
 DATABASE_URL = "postgres://user:password@localhost:5432/mydatabase"
 
+# Define the database models
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
