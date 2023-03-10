57a500a075993071d0b9728caed32a32ced5f535
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2023-03-10 09:48:02 +0100

    Update depthAI_capture.py

--- depthAI_capture.py
+++ depthAI_capture.py
@@ -18,7 +18,6 @@ xout_rgb = pipeline.create(depthai.node.XLinkOut)
 xout_rgb.setStreamName("rgb")
 cam_rgb.preview.link(xout_rgb.input)
 
-        
 #         if frame is not None:
 #             cv2.imshow("preview", frame)
 #             if cv2.waitKey(1) == ord('q'):
