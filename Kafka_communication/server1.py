05c83a042e61efca30b971f5eeabde488ae350ff
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-03-14 11:56:13 +0100

    Update server1.py

--- Kafka_communication/server1.py
+++ Kafka_communication/server1.py
@@ -2,4 +2,5 @@ from fastapi import FastAPI
 from confluent_kafka import Producer, Consumer
 
 app = FastAPI()
+
 producer = Producer({'bootstrap.servers': 'kafka-broker-a:9092,kafka-broker-b:9092'})
