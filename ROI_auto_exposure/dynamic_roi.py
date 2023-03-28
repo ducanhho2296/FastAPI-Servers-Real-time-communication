6a82fd6c710cb8274f11400e8c4ce3ba28b43384
novoai <92146886+ducanhho2296@users.noreply.github.com>
2023-03-28 09:58:17 +0200

    Update dynamic_roi.py

--- ROI_auto_exposure/dynamic_roi.py
+++ ROI_auto_exposure/dynamic_roi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import cv2
